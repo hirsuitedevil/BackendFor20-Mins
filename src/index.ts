b69dbdaccc28708dcf23a51bf0c08cf57ae738fb
Animesh-03 <anivad03@gmail.com>
2023-09-29 23:39:29 +0530

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import express from 'express';
 import axios from 'axios';
 import { decode } from './polylineUtils';
+import cors from "cors"
 const app = express();
 const port = 5000;
 
+app.use(cors())
+
 app.get('/', async (req, res) => {
+  console.log("AAAAAAAAAAA")
   try {
     const { loc, time, transport, service } = req.query;
     const apiKey = '7UalH_mc8-4SsygiyEDs5Y9FWsK9xsSDSA0hUPZW2lw';
     const timeInSec = 60 * Number(time);
+
+    console.log("Called")
 
     // Request isoline data
     const apiURLIsoline = `https://isoline.router.hereapi.com/v8/isolines?transportMode=${transport}&range[type]=time&range[values]=${timeInSec}&origin=${loc}&apikey=${apiKey}`;
@@ -17,11 +23,15 @@ app.get('/', async (req, res) => {
     // Decode polyline data
     const outerPolygon = responseIsoline.data.isolines[0].polygons[0].outer;
     const decodedPolygon = decode(outerPolygon);
+
+    console.log("Decoded")
     
 
     // Request geocoding data
     const apiURLGeoCoding = `https://discover.search.hereapi.com/v1/discover?at=${loc}&q=${service}&apiKey=${apiKey}&limit=100`;
     const responseGeoCoding = await axios.get(apiURLGeoCoding);
+
+    console.log("Filtering")
 
     const pos = responseGeoCoding.data.items.filter((i:any)=>{
         function pointInPolygon(polygon : any, point : any) {
@@ -37,10 +47,11 @@ app.get('/', async (req, res) => {
         };
         return pointInPolygon(decodedPolygon.polyline,[i.position.lat,i.position.lng])
     })
-    res.json(pos)
+    console.log("Filtered")
+    res.send(pos);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).send({ error: 'Internal Server Error' });
   }
 });
 
